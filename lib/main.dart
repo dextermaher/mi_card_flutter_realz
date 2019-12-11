a2673db6c4fafcd4479301d35600fa610291c66b
dextermaher <dexter@maher-family.net>
2019-12-10 20:22:01 -0800

    working-ish

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,14 @@ class MyApp extends StatelessWidget {
           child: Column(
             children: <Widget>[
               CircleAvatar(
-                  radius: 80.0,
-                  backgroundColor: Colors.transparent,
-                  backgroundImage: AssetImage('images/thetruth.png')),
+                radius: 80.0,
+                backgroundColor: Colors.transparent,
+                backgroundImage: AssetImage('images/thetruth.png'),
+              ),
+//              Transform.rotate(
+              // dexter: remove me... bring Text child up here instead
+//                angle: -3.14 / 15.0,
+//                child:
               Text(
                 'Wake Up America',
                 style: TextStyle(
@@ -26,6 +31,61 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
+//              ),
+              Text(
+                'Bird Watching Goes Both Ways',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontFamily: 'Railway',
+                  color: Colors.black,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+              Card(
+                color: Colors.black,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+//                child: Padding(
+//                  padding: const EdgeInsets.all(15.0),
+//              ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.launch,
+                    color: Colors.teal.shade400,
+                  ),
+                  title: Text(
+                    'BirdsArentReal.com',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.teal.shade400,
+                      fontFamily: 'Railway',
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.black,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.looks,
+                        color: Colors.teal.shade400,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text('Visit Our Merch Shop',
+                          style: TextStyle(
+                            color: Colors.teal.shade400,
+                            fontFamily: 'Railway',
+                            fontSize: 20.0,
+                          ))
+                    ],
+                  ),
+                ),
+              )
             ],
           ),
         ),
@@ -39,3 +99,20 @@ class MyApp extends StatelessWidget {
 //color: Colors.white,
 //child: Text('Container 1'))
 //
+
+//
+//children: <Widget>[
+//Icon(
+//Icons.launch,
+//color: Colors.teal.shade400,
+//),
+//SizedBox(
+//width: 10.0,
+//),
+//Text('BirdsArentReal.com',
+//style: TextStyle(
+//color: Colors.teal.shade400,
+//fontFamily: 'Railway',
+//fontSize: 20.0,
+//))
+//],
